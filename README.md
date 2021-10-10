6406d9a7281701183a95a32c6b09e5fdea82a990
Luis Estrada <lestradav2@hotmail.com>
2021-10-10 13:54:34 -0500

    Añadí el index.html con links al resto de documentos

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # platzi-curso-practico-javascript
+
+...
+
+##Taller #1 figuras geométricas
+
+-Primer paso: definir las formulas
+-Segundo paso: implementar las fórmulas en Javascript
+-Tercer pso: crear funciones 
+-Cuarto paso: integrar JS con HTML
+
+## Taller #2: porcentajes y descuentos
+
+-Primer paso: definir las formulas
+-Segundo paso: implementar las formulas en JavaScript
+-Tercer paso: Crear funciones
+-Cuarto paso: integrar JS con HTML
+
+## Taller #3: promedio, mediana y moda
+
+-Primer paso: definir las formulas
+-Segundo paso: implementar las formulas en JavaScript
+-Tercer paso: Crear funciones
+-Cuarto paso: integrar JS con HTML
---
+++ salarios.js
@@ -0,0 +1,52 @@
+const colombia = [];
+colombia.push({
+    name:"Camila",
+    salary: 500,
+});
+colombia.push({
+    name:"Nath",
+    salary: 1500,
+});
+colombia.push({
+    name:"Luisa",
+    salary: 1800,
+});
+colombia.push({
+    name:"Luis",
+    salary: 2300,
+});
+colombia.push({
+    name:"Ricki",
+    salary: 4300,
+});
+colombia.push({
+    name:"RLaura",
+    salary: 2100,
+});
+colombia.push({
+    name:"Eduardo",
+    salary: 2050,
+});
+colombia.push({
+    name:"Julian",
+    salary: 750,
+});
+colombia.push({
+    name:"Maria",
+    salary: 550,
+});
+colombia.push({
+    name:"Sofia",
+    salary: 3333,
+});
+colombia.push({
+    name:"Mayela",
+    salary: 3343,
+});
+colombia.push({
+    name:"Jose",
+    salary: 1343,
+});colombia.push({
+    name:"Manuel",
+    salary: 1343,
+});
